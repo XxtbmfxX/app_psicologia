c21edee4caa92b328dbf225797ee08d2aac94414
Raimundo <raimundo.romero@inacapmail.cl>
2024-11-22 12:03:13 -0300

    Lista muestra apellidos y rut

--- components/mostrar/FilaPaciente.tsx
+++ components/mostrar/FilaPaciente.tsx
@@ -5,9 +5,11 @@ import { Link, router } from "expo-router";
 type FilaPacienteProps = {
   id: string;
   nombre: string;
+  apellido: string;
+  rut:  string;
 };
 
-const FilaPaciente: React.FC<FilaPacienteProps> = ({ id, nombre }) => {
+const FilaPaciente: React.FC<FilaPacienteProps> = ({ id, nombre, apellido, rut }) => {
   return (
     <Link
       className=" bg-gray-50 border-gray-400 border-b rounded-lg shadow-md mb-3 "
@@ -16,9 +18,10 @@ const FilaPaciente: React.FC<FilaPacienteProps> = ({ id, nombre }) => {
         params: { id: id },
       }}
     >
-      <View className="p-4">
-        <Text className="text-lg font-semibold text-blue-600">{nombre}</Text>
-      </View>
+      <View className="p-3">
+        <Text className="text-lg font-semibold text-blue-600">{nombre} {apellido}</Text>    
+        <Text className="text-sm font-semibold text-blue-400 mt-2">{rut}</Text>
+        </View>
     </Link>
   );
 };
